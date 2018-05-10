c7ec686704ddb2eac12fab6ecaad6e3ba8ab22af
Sandbom <simon.sandbom@gmail.com>
2018-05-10 19:08:34 +0200

    Added healthbar for enemy

--- UI Programming II/Assets/Game/Scripts/EnemyHealth.cs
+++ UI Programming II/Assets/Game/Scripts/EnemyHealth.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyHealth : MonoBehaviour {
 
 	public float enemyMaxHealth;
+
+	public GameObject enemyDeathFX;
+	public Slider enemyhealthbar;
 
 	float currentHealth;
 
 	// Use this for initialization
 	void Start () {
 		currentHealth = enemyMaxHealth;	
+		enemyhealthbar.maxValue = currentHealth;
+		enemyhealthbar.value = currentHealth;
 		
 	}
 	
@@ -20,13 +26,18 @@ public class EnemyHealth : MonoBehaviour {
 	}
 
 	public void addDamage(float damage){
+
+		enemyhealthbar.gameObject.SetActive (true);
 		currentHealth = currentHealth - damage;
+		enemyhealthbar.value = currentHealth;
+
 		if (currentHealth <= 0) {
 			killEnemy ();
 		}
 	}
 
 	void killEnemy(){
+		Instantiate (enemyDeathFX, transform.position, transform.rotation);
 		Destroy (gameObject);
 	}
 }
