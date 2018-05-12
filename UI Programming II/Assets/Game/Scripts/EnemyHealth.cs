2fe9ffe9526d7c38907cf105a209bc4f1aba06cc
Sandbom <simon.sandbom@gmail.com>
2018-05-12 13:45:00 +0200

    skeleton death animation and bug fixes

--- UI Programming II/Assets/Game/Scripts/EnemyHealth.cs
+++ UI Programming II/Assets/Game/Scripts/EnemyHealth.cs
@@ -10,14 +10,19 @@ public class EnemyHealth : MonoBehaviour {
 	public GameObject enemyDeathFX;
 	public Slider enemyhealthbar;
 
+	Animator enemyAnim;
+
 	float currentHealth;
+	public bool alive = true;
+	Rigidbody2D enemyRB;
 
 	// Use this for initialization
 	void Start () {
 		currentHealth = enemyMaxHealth;	
 		enemyhealthbar.maxValue = currentHealth;
 		enemyhealthbar.value = currentHealth;
-		
+		enemyAnim = GetComponentInChildren<Animator> ();
+		enemyRB = GetComponentInParent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
@@ -26,18 +31,21 @@ public class EnemyHealth : MonoBehaviour {
 	}
 
 	public void addDamage(float damage){
-
 		enemyhealthbar.gameObject.SetActive (true);
 		currentHealth = currentHealth - damage;
 		enemyhealthbar.value = currentHealth;
 
 		if (currentHealth <= 0) {
-			killEnemy ();
+			enemyAnim.SetBool ("EnemyDeath", true);
+			alive = false;
+			//enemyRB.velocity = new Vector2 (0f, 0f);
+			Invoke("killEnemy", 1);
 		}
 	}
 
 	void killEnemy(){
-		Instantiate (enemyDeathFX, transform.position, transform.rotation);
+		//Instantiate (enemyDeathFX, transform.position, transform.rotation);
 		Destroy (gameObject);
 	}
+		
 }
