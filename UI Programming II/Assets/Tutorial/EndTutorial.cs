cc218bada40b33676de32bca0f76c0ce4d90efa7
Sandbom <simon.sandbom@gmail.com>
2018-06-08 02:20:38 +0200

    fixed tutorial ending bug

--- UI Programming II/Assets/Tutorial/EndTutorial.cs
+++ UI Programming II/Assets/Tutorial/EndTutorial.cs
@@ -5,14 +5,19 @@ using TMPro;
 
 public class EndTutorial : MonoBehaviour {
 
-	public GameObject tutorialCanvas;
+
+	public GameObject menuButton;
+	//public GameObject quitButton;
+	//public GameObject tutendText;
 
 
 	// When entering this collider 
 	// we show the canvas letting player exit to main menu and we slow down time giving a slow motion effect
 	void OnTriggerEnter2D(){
-		Time.timeScale = 0.05f;
+		Time.timeScale = 0.02f;
 		Time.fixedDeltaTime = 0.02F * Time.timeScale;
-		tutorialCanvas.SetActive (true);
+		menuButton.SetActive (true);
+		//quitButton.SetActive (true);
+		//tutendText.SetActive (true);
 	}
 }
